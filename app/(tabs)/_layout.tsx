f869000e3116259c32f13bdeaf75d88127da6fc8
cnicholas01 <nicholas.ckchan@gmail.com>
2026-01-06 21:15:15 +0800

    Expo bottom tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,36 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
+import { Tabs } from "expo-router";
+import { View } from "react-native";
+import "../../global.css";
+import {Feather} from '@expo/vector-icons';
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+    <Tabs screenOptions={{tabBarActiveTintColor: 'black', tabBarShowLabel: false}}>
+      <Tabs.Screen name="index" options={{ 
+        title: "Home",
+        tabBarIcon: ({color, size}) => (
+          <Feather name="home" color={color} size={size} />
+        )
+        }} />
+          <Tabs.Screen name="search" options={{ 
+            title: "Search",
+            tabBarIcon: ({color, size}) => (
+              <Feather name="search" color={color} size={size} />
+            ) }} />
+
+      <Tabs.Screen name="notifications" options={{ 
+        title: "Notifications",
+        tabBarIcon: ({color, size}) => (
+          <Feather name="heart" color={color} size={size} />
+        ) 
+        }} />
+      <Tabs.Screen name="profile" options={{
+         title: "Profile",
+         tabBarIcon: ({color, size}) => (
+          <Feather name="user" color={color} size={size} />
+        )  }} />
+
     </Tabs>
   );
 }
+
+//name of tab must be the same as the file name
---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+import "../../global.css";
+export default function Profile() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>Profile</Text>
+    </View>
+  );
+}
